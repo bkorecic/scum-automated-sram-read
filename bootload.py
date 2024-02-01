bc6c6f27abf0db64c8ce28806549ea1a0122cb18
bkorecic <blaz.korecic@ug.uchile.cl>
2024-02-01 14:49:16 +0100

    Initial commit

---
+++ bootload.py
@@ -0,0 +1,56 @@
+from serial_interface import SerialInterface
+
+import random
+
+def bootload():
+    # Serial connections
+    nRF_ser = None
+    #uart_ser = None
+
+    nRF_port = "/dev/ttyACM3"
+    #binary_image = "/home/bkorecic/aio/scum-test-code/scm_v3c/applications/extracting_secret_key/ecc/repetition_code/read_key/Objects/read_key.bin"
+    binary_image = "/home/bkorecic/aio/scum-test-code/scm_v3c/applications/extracting_secret_key/ecc/bch/helper_data_gen/Objects/helper_data_gen.bin"
+    #boot_mode = '3wb'
+    pad_random_payload = False
+
+    # Open COM port to nRF
+    nRF_ser = SerialInterface(
+        port=nRF_port,
+        baudrate=250000)
+
+    # Open binary file from Keil
+    with open(binary_image, 'rb') as f:
+        bindata = bytearray(f.read())
+
+    # Need to know how long the binary payload to pad to 64kB
+    code_length = len(bindata) - 1
+    pad_length = 65536 - code_length - 1
+
+    # print(code_length)
+
+    # Optional: pad out payload with random data if desired
+    # Otherwise pad out with zeros - uC must receive full 64kB
+    if (pad_random_payload):
+        for _ in range(pad_length):
+            bindata.append(random.randint(0, 255))
+        code_length = len(bindata) - 1 - 8
+    else:
+        for _ in range(pad_length):
+            bindata.append(0)
+
+
+    # Transfer payload to nRF
+    # nRF_ser.write(b'transfersram\n')
+    # print(nRF_ser.read_until())
+    # Send the binary data over uart
+    nRF_ser.write(bindata)
+    # and wait for response that writing is complete
+    # print(nRF_ser.read())
+    nRF_ser.read()
+    nRF_ser.read()
+
+    # Execute 3-wire bus bootloader on nRF
+    # nRF_ser.write(b'boot3wb\n')
+
+    # Display 3WB confirmation message from nRF
+    #print(nRF_ser.read())
---
+++ main.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+from datetime import date
+from bootload import bootload
+import os
+import sys
+import serial
+import time
+import subprocess
+import re
+import csv
+
+
+def main():
+    uart_port = "/dev/ttyUSB0"
+
+    correct_key = "683d02f7e2fa18fd"
+
+    results_path = 'results/' + date.today().isoformat()
+    results_writer = csv.writer(open(results_path, 'w', newline=''))
+
+    successes = 0
+    failures = 0
+    retries = 0
+    for i in range(1000):
+        # unplug the usb cables (SRAM is totally turned off)
+        subprocess.run(["/usr/bin/ykushcmd", "-d", "a"], stdin=None,
+                       stdout=None, stderr=None, shell=False)
+        # wait for the SRAM cells to get their default values
+        time.sleep(5)
+        # plug the usb cables
+        subprocess.run(["/usr/bin/ykushcmd", "-u", "a"], stdin=None,
+                       stdout=None, stderr=None, shell=False)
+        # wait for the nRF to bootload
+        time.sleep(10)
+
+        # flash the firmware into the SCuM chip through the nRF
+        # the nRF will send back a confirmation when done
+        try:
+            subprocess.run(
+                ["python3", "/home/bkorecic/aio/SCuM-programmer/test_bootload.py"],
+                stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
+        except subprocess.CalledProcessError:
+            retries += 1
+            continue
+
+        # open the serial port with SCuM
+        uart_ser = serial.Serial(
+            port=uart_port,
+            baudrate=19200,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS)
+
+        if uart_ser.is_open != 1:
+            uart_ser.open()
+
+        # read the output of the firmware running on SCuM
+        # print('Reading the serial port.')
+        while uart_ser.is_open:
+            data = uart_ser.readline()
+            match = re.search(b'Key: "(.+?)"', data)
+            if match:
+                key = match.group(1).decode('utf-8')
+                if key == correct_key:
+                    successes += 1
+                else:
+                    failures += 1
+                print(
+                    f"Successes: {successes}, failures: {failures}, retries: {retries}")
+            # when finishing this round close the serial port with SCuM to allow
+            # the nRF to use it later
+            if data == b'TEST DONE\r\n':
+                # print("Closing the serial port.")
+                uart_ser.close()
+
+
+if __name__ == '__main__':
+    if os.geteuid() != 0:
+        print('Must run as root')
+        sys.exit(1)
+    main()
